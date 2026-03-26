45c5e18923cfa7ef783d23e5f63883786a3ffe1d
yesh20hiya-afk <yesh20hiya@gmail.com>
2026-03-25 21:28:16 -0400

    v2

---
+++ activity2.py
@@ -0,0 +1,31 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(300,400)
+
+board = turtle.Turtle()
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+
+board.right(150)
+board.forward(50)
+
+board.pendown()
+
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
+
